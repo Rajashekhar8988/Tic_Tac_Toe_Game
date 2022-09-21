b840851009ab3cc2383afdd5f5e98fdda857c895
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-21 08:37:57 +0530

    printing the char elements

---
+++ TicTacToe.java
@@ -0,0 +1,13 @@
+package TicTacToeGame;
+
+public class TicTacToe {
+	public static void TicTacToeGame(){
+		char[] Array = {'a','b','c','d','e','f','g','h','i','j'};
+		for (int i=0; i<Array.length; i++) {
+		System.out.print(Array[i]+" ");
+		}
+	}
+public static void main(String[] args) {
+	TicTacToe.TicTacToeGame();
+}
+}
