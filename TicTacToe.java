533ba491e6bdb2a0af1d306ec2438a56aa9fd688
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-21 08:56:02 +0530

    printing the char elements of size 10

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,13 +1,16 @@
 package TicTacToeGame;
 
 public class TicTacToe {
+	
 	public static void TicTacToeGame(){
-		char[] Array = {'a','b','c','d','e','f','g','h','i','j'};
-		for (int i=0; i<Array.length; i++) {
+		char[] Array =new char[10];
+		for (int i=1; i<Array.length; i++) 
+		{
 		System.out.print(Array[i]+" ");
 		}
 	}
 public static void main(String[] args) {
-	TicTacToe.TicTacToeGame();
+	System.out.println("WELCOME TO TIC TAC TOE GAME");
+	TicTacToeGame();
 }
 }
